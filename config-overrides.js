f221ad2c9011739f8b98aa5f8ef1ab05fa3980ab
krakenon <luonguit@gmail.com>
2021-08-31 14:56:52 +0700

    update config

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,19 @@
 const { override, addLessLoader, addWebpackExternals } = require('customize-cra');
 
+const overrideWebpackProductionExternals = (extenals) => {
+    if (process.env.NODE_ENV === 'production') {
+        return addWebpackExternals(extenals);
+    }
+
+    return addWebpackExternals({});
+};
+
 module.exports = override(
-  addLessLoader({
-    javascriptEnabled: true,
-  }),
-  addWebpackExternals({
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  })
+    addLessLoader({
+        javascriptEnabled: true,
+    }),
+    overrideWebpackProductionExternals({
+        react: 'React',
+        'react-dom': 'ReactDOM',
+    })
 );
